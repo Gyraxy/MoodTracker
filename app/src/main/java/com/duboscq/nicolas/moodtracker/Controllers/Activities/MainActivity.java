55b0f54b166fb34563f3a960230ec27efe1f84a9
Gyraxy <nicolasduboscq@hotmail.com>
2018-06-13 15:41:31 +0200

    Initial Project

---
+++ app/src/main/java/com/duboscq/nicolas/moodtracker/Controllers/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.duboscq.nicolas.moodtracker.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duboscq.nicolas.moodtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
